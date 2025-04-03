8c4f337895d48d4febcdb4cc4f1877eae744438d
Witold <witek99.pl@gmail.com>
2025-04-03 13:24:07 +0200

    Added handleDriver Added mouseClicked To ClicksConverter

--- src/main/java/edu/kis/powp/jobs2d/features/ClicksConverter.java
+++ src/main/java/edu/kis/powp/jobs2d/features/ClicksConverter.java
@@ -40,4 +40,23 @@ public class ClicksConverter extends MouseAdapter {
 
         return new Point(x - offsetX, y - offsetY);
     }
+
+    private void handleDriver(Point position, int buttonPressed) {
+        Job2dDriver driver = DriverFeature.getDriverManager().getCurrentDriver();
+
+        if(buttonPressed == MOUSE_BUTTON_LEFT) {
+            driver.operateTo(position.getX(), position.getY());
+        }
+
+        if (buttonPressed == MOUSE_BUTTON_RIGHT) {
+            driver.setPosition(position.getX(), position.getY());
+        }
+    }
+
+    public void mouseClicked(MouseEvent event) {
+        Point position = getClickPosition(event);
+        int buttonPressed = event.getButton();
+
+        handleDriver(position, buttonPressed);
+    }
 }
